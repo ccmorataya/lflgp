5b2affa3f92d64b96ac4515e707bbbf075f25233
Cristian Morataya <cris.morataya@gmail.com>
2019-09-19 14:07:01 -0600

    Add touch input matrix movement like ESDF (WASD)

--- shapes/main.lua
+++ shapes/main.lua
@@ -1,5 +1,6 @@
 -- variables
 local x, y = 0
+MOVEMENT = 300
 
 function love.load() -- loads all we need in game
   -- create character table (rectangle shape) with initial position defined in the table
@@ -15,7 +16,8 @@ end
 function love.draw() -- function to display/draw content to screen
   -- debug
   love.graphics.print("CM::x_touch: " .. tostring(x), 10, 10)
-  love.graphics.print("CM::character_x: " .. tostring(character.x), 10, 30)
+  love.graphics.print("CM::y_touch: " .. tostring(y), 10, 30)
+  love.graphics.print("CM::character_x: " .. tostring(character.x), 10, 50)
 
   love.graphics.rectangle("fill", character.x, character.y, 50, 50)
 end
@@ -23,16 +25,21 @@ end
 function love.update(dt)
   -- store the touch screen interaction
   local touches = love.touch.getTouches()
+  local oneThree = love.graphics.getWidth() / 3
 
   for i, id in ipairs(touches) do
     x, y = love.touch.getPosition(id)
 
     -- on right press, rotate to right
-    if x > ( love.graphics.getWidth() /2 ) then
-      character.x = character.x + 100 * dt
+    if x >= ( oneThree * 2 ) and x < love.graphics.getWidth() and y > love.graphics.getHeight() / 2 then
+      character.x = character.x + MOVEMENT * dt
       -- on left press, rotate to left
-    elseif x < ( love.graphics.getWidth() /2 ) then
-      character.x = character.x - 100 * dt
+    elseif x < oneThree and y > love.graphics.getHeight() / 2 then
+      character.x = character.x - MOVEMENT * dt
+    elseif x > oneThree and x < oneThree * 2 and y < love.graphics.getHeight() / 2 then
+      character.y = character.y - MOVEMENT * dt
+    elseif x > oneThree and x < oneThree * 2 and y > love.graphics.getHeight() / 2 then
+      character.y = character.y + MOVEMENT * dt
     end
   end
 
